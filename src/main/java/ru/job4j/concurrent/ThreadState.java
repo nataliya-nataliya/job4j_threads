43b2558d0bff7c5dc1bf1849d907c467cd7c5b86
nataliya-nataliya <nataliya.chest@gmail.com>
2023-05-24 13:40:38 +0400

    3. State of the thread. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        System.out.println(first.getName() + " " + first.getState());
+        first.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+        }
+        System.out.println(first.getName() + " " + first.getState());
+        Thread second = new Thread(
+                () -> {
+                }
+        );
+        System.out.println(second.getName() + " " + second.getState());
+        second.start();
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getName() + " " + second.getState());
+        }
+        System.out.println(second.getName() + " " + second.getState());
+        System.out.println("Program completed");
+    }
+}
