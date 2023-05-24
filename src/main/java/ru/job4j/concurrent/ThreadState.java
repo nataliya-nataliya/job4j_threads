20774d348c129ae4a57c31406e35c5c636ad606a
nataliya-nataliya <nataliya.chest@gmail.com>
2023-05-24 15:26:50 +0400

    merging into 1 cycle

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -6,21 +6,19 @@ public class ThreadState {
                 () -> {
                 }
         );
-        System.out.println(first.getName() + " " + first.getState());
-        first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getName() + " " + first.getState());
-        }
-        System.out.println(first.getName() + " " + first.getState());
         Thread second = new Thread(
                 () -> {
                 }
         );
+        System.out.println(first.getName() + " " + first.getState());
         System.out.println(second.getName() + " " + second.getState());
+        first.start();
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
             System.out.println(second.getName() + " " + second.getState());
         }
+        System.out.println(first.getName() + " " + first.getState());
         System.out.println(second.getName() + " " + second.getState());
         System.out.println("Program completed");
     }
