c06b0e449da187c84bddc9a15746f3341955ff31
nataliya-nataliya <nataliya.chest@gmail.com>
2023-06-04 01:05:07 +0400

    replaced amount condition

--- src/main/java/ru/job4j/cache/AccountStorage.java
+++ src/main/java/ru/job4j/cache/AccountStorage.java
@@ -28,13 +28,13 @@ public class AccountStorage {
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount can't be less than or equal to 0");
+        }
         boolean isTransfer = false;
         Optional<Account> accFromId = getById(fromId);
         Optional<Account> accToId = getById(toId);
         if (accFromId.isPresent() && accToId.isPresent()) {
-            if (amount <= 0) {
-                throw new IllegalArgumentException("Amount can't be less than or equal to 0");
-            }
             if (accFromId.get().amount() >= amount && fromId != toId) {
                 Account sourceAcc = new Account(fromId, accFromId.get().amount() - amount);
                 Account targetAcc = new Account(toId, accToId.get().amount() + amount);
